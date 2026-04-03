9c2291da85c1cb33855eaeafb19c9fb9ac286981
Victor Elgersma <elgersmav@gmail.com>
2026-04-03 12:58:57 +0200

    moved quotes into separate file

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html lang="en">
 
@@ -48,11 +50,11 @@
         <p>Thanks for 🏄 surfing by, stranger!</p>
         <section>
             <h2>Writing</h2>
-            <p><a href="https://vjbe.net/pdfs/2026/03/lhopital-2.pdf" target="_blank">The Origins of L'Hôpital's Rule </a><span
+            <p><a href="https://vjbe.net/pdfs/2026/03/lhopital-2.pdf" target="_blank">How The Marquis de L'Hôpital bought his famous "Rule" from Johann Bernoulli </a><span
                     class="marginnote">Report for "History of Mathematics Seminar" taught by <a
                         href="https://www.uu.nl/staff/VNEBlasjo">Dr. Viktor Blåsjö</a></span> (March 2026)
             </p>
-            <p><a href="https://vjbe.net/pdfs/2026-03-18-Longitude-6.pdf" target="_blank">The Harrison H1 chronometer </a><span
+            <p><a href="https://vjbe.net/pdfs/2026-03-18-Longitude-6.pdf" target="_blank">The First Sea-clock (Harrison H1 Chronometer, 1735) </a><span
                     class="marginnote">Essay for "Science and the Public" taught by <a
                         href="https://www.uu.nl/staff/RBWille">Dr. R.B. Wille</a></span> (March 2026)
             </p>
@@ -78,7 +80,10 @@
                             tree</a></p>
                 </li>
                 <li>
-                    <a href="https://ishtarthemovie.com/downloads/RogersAndClarke-WardrobeOfLove.mp3" target="_blank">Ishtar!</a>
+                    <a href="https://ishtarthemovie.com/downloads/RogersAndClarke-DangerousBusiness.mp3" target="_blank">Ishtar!</a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/watch?v=XRu5kjIa3-0" target="_blank">Orangutan Driving a Golf Cart </a>
                 </li>
             </ul>
 
@@ -92,21 +97,9 @@
         <p style="font-style: italic;">View <a href="https://github.com/victorelgersma/personal_home_page" target="_blank">Source
                 Code 🧑‍💻</a></p>
 
-        <?php
-        $quotes = [
-            [
-                "text" => "The book of nature is written in the language of mathematics.",
-                "author" => "Roger Bacon"
-            ],
-            [
-                "text" => "Plato is my friend. Aristotle is my friend. But my biggest friend is the truth",
-                "author" => "Isaac Newton"
-            ],
-            [
-                "text" => "Hey, it takes a lot of nerve to have nothing at your age. Don't you understand that? Yeah, most guys would be ashamed, but you've got the guts to just say, 'The hell with it.' You say that you'd rather have nothing than settle for less. Understand?",
-                "author" => "Lyle Rogers (Ishtar)"
-            ],
-        ];
+
+    <?php 
+        $quotes = require 'quotes.php';
 
         // Pick a random quote
         $quote_of_the_day = $quotes[array_rand($quotes)];
---
+++ quotes.php
@@ -0,0 +1,22 @@
+
+<?php
+$quotes = [
+    [
+        "text" => "The book of nature is written in the language of mathematics.",
+        "author" => "Roger Bacon"
+    ],
+    [
+        "text" => "Plato is my friend. Aristotle is my friend. But my biggest friend is the truth",
+        "author" => "Isaac Newton"
+    ],
+    [
+        "text" => "But with regard to the material world, we can at least go so far as this— we can perceive that events are brought about not by insulated interpositions of Divine power, exerted in each particular case, but by the establishment of general laws",
+        "author" => "Whewell: Bridgewater Treatise"
+    ],
+    [
+        "text" => "have plants any notion of cause & effect?",
+        "author" => "Charles Darwin, Unpublished Notebooks (Howard Gruber, Darwin on Man: A Psychological Study of Scientific Creativity, with Paul Barrett (ed.), Darwin’s Early and Unpublished Notebooks (London, 1974), 332, 333, 443.)",
+    ]
+];
+
+return $quotes;
