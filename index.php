fe9a65dc9d68211f55e5e4083e41acedfce54946
Victor Elgersma <elgersmav@gmail.com>
2026-04-08 00:07:02 +0200

    updated reading

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <title>Victor Elgersma</title>
     <link rel="stylesheet" href="/tufte.css" />
     <style>
-
         .email-link {
             font-weight: bold;
             text-decoration: none;
@@ -29,38 +28,33 @@
         <p>Thanks for 🏄 surfing by, stranger!</p>
         <section>
             <h2>Writing</h2>
-            <p><a href="https://vjbe.net/pdfs/2026/2026-04-05-The%20Origins%20of%20the%20L%27H%C3%B4pital-Bernoulli%20Rule%20%28Report%29.pdf-6" target="_blank">L'Hôpital's Rule: Origins</a><span class="marginnote">Report for "History of
-                    Mathematics Seminar" taught by <a href="https://www.uu.nl/staff/VNEBlasjo">Dr. Viktor
-                        Blåsjö</a></span> (March 2026)
+            <p><a href="https://vjbe.net/pdfs/2026/2026-04-08-The%20Origins%20of%20the%20L%27H%C3%B4pital-Bernoulli%20Rule%20%28Report%29-2.pdf"
+                    target="_blank">L'Hôpital's Rule: Origins (1692-1696)</a><span class="marginnote">(April 2026)
+                </span>
             </p>
-            <p><a href="https://vjbe.net/pdfs/2026-03-18-Longitude-6.pdf" target="_blank">The First Sea-clock (Harrison
-                    H1 Chronometer, 1735) </a><span class="marginnote">Essay for "Science and the Public" taught by <a
-                        href="https://www.uu.nl/staff/RBWille">Dr. R.B. Wille</a></span> (March 2026)
+            <span>A line-by-line commentary of Chapter 9 of L’Hôpital’s
+                <em> Analyse </em>(1696) and a letter from Bernoulli to
+                L’Hôpital from July 1694.
+            </span>
+            <br>
+            <span>Report for
+                "History of
+                Mathematics Seminar" taught by <a href="https://www.uu.nl/staff/VNEBlasjo">Dr. Viktor
+                    Blåsjö</a>
+            </span>
+            <p><a href="https://vjbe.net/pdfs/2026-03-18-Longitude-6.pdf" target="_blank">The Harrison
+                    H1 Chronometer (1735) </a><span class="marginnote">(March 2026) </span>
             </p>
+            <span>
+                How John Harrison's mechanical genius revolutionised navigation despite the scepticism of the scientific
+                elite.
+            </span> <br>
+            <span>Essay for "Science and the Public"
+                taught by <a href="https://www.uu.nl/staff/RBWille">Dr. R.B. Wille</a></span>
         </section>
 
 
-        <section>
-            <h2>Miscellaneous</h2>
-            <ul>
-                <li>
-                    <a href="https://fr.wikipedia.org/wiki/Arganier#/media/Fichier:Huile_d'argan_003.JPG"
-                        target='_blank'>Goats in an
-                        argan
-                        tree</a>
-                </li>
-                <li>
-                    <a href="https://ishtarthemovie.com/downloads/RogersAndClarke-DangerousBusiness.mp3"
-                        target="_blank">Ishtar!</a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/watch?v=XRu5kjIa3-0" target="_blank">Orangutan Driving a Golf Cart
-                    </a>
-                </li>
-            </ul>
 
-
-        </section>
         <section>
             <h2>Videos</h2>
             <ul>
@@ -77,17 +71,38 @@
             </ul>
         </section>
 
+        <section>
+            <h2>Just for Fun</h2>
+            <ul>
+                <li>
+                    <a href="https://fr.wikipedia.org/wiki/Arganier#/media/Fichier:Huile_d'argan_003.JPG"
+                        target='_blank'>Goats in an
+                        argan
+                        tree</a>
+                </li>
+                <li>
+                    <a href="https://ishtarthemovie.com/downloads/RogersAndClarke-DangerousBusiness.mp3"
+                        target="_blank">Ishtar!</a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/watch?v=DJsn1QivbKM" target="_blank">Orangutan Driving a Golf Cart
+                    </a>
+                </li>
+            </ul>
 
+
+        </section>
     </article>
+
 
     <footer>
         <section>
-                <h2>Contact</h2>
-                <p>
-                    <a class="email-link" href="mailto:v.j.b.elgersma@students.uu.nl">
-                        v.j.b.elgersma@students.uu.nl
-                    </a>
-                </p>
+            <h2>Contact</h2>
+            <p>
+                <a class="email-link" href="mailto:v.j.b.elgersma@students.uu.nl">
+                    v.j.b.elgersma@students.uu.nl
+                </a>
+            </p>
         </section>
 
         <section>
--- quotes.php
+++ quotes.php
@@ -15,7 +15,11 @@ $quotes = [
     ],
     [
         "text" => "have plants any notion of cause & effect?",
-        "author" => "Charles Darwin, Unpublished Notebooks (Howard Gruber, Darwin on Man: A Psychological Study of Scientific Creativity, with Paul Barrett (ed.), Darwin’s Early and Unpublished Notebooks (London, 1974), 332, 333, 443.)",
+        "author" => "Charles Darwin, Unpublished Notebooks (Howard Gruber, Darwin on Man: A Psychological Study of Scientific Creativity, with Paul Barrett (ed.), Darwin's Early and Unpublished Notebooks (London, 1974), 332, 333, 443.)",
+    ],
+    [
+        "text" => "'Is there one Professor in Edinburgh pursuing science with zeal? Are they not all occupied as showmen whose principal object is to attract pupils and make money?'",
+        "author" => "David Brewster to J. D. Forbes, 11 February 1830, J. R. R. Christie (ed.), Martyr of Science: Sir David Brewster 1781-1868 (1984), 13."
     ]
 ];
 
